e5eced7030b48b034e2f850c2df2a29781363fda
LARDIER Rémi <remi.lardier@epsi.fr>
2019-03-21 12:42:02 +0100

    AddDockerfileAndcopyCode

--- script.groovy
+++ script.groovy
@@ -1,20 +1,5 @@
 #!groovy
 
-pipeline {
-  agent none
-  stages {
-    stage('Git clone') {
-      agent any
-      steps {
-        sh 'git clone https://github.com/Akasam/samplephpwebsite'
-        sh 'git checkout v2'
-      }
-    }
-    stage('Docker Build') {
-      agent any
-      steps {
-        sh 'docker build -t samplewebsite.php .'
-      }
-    }
-  }
+step{
+    shell('ls')
 }
